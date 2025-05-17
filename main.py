fad571d5669110a1d96e94456ca746102dd15c9c
BodeMurairi2 <b.murairi@alustudent.com>
2025-05-17 21:40:16 +0200

    Urls found

--- main.py
+++ main.py
@@ -4,15 +4,30 @@ This module uses regular expression to extract data
 """
 import re
 
+# open form.txt
+with open("form.txt", "r") as file:
+    file_data = file.read()
+
+# email regex form to extract all emails
 email_regex = r"\b[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z0-9._]{2,}\b"
 
-with open("forms.txt", "r") as file:
-    data = file.read()
+def email_find(email_regex, file_data):
+    """This function display all the emails found"""
+    email_match = re.findall(email_regex, file_data)
+    if email_match:
+        for emails in email_match:
+            print(emails)
+    else:
+        print("No email founds")
 
-email_match = re.findall(email_regex, data)
+#.........................................
+#  Finding URL
+url_regex = r"https?:\/\/(?:www\.)?[a-zA-Z0-9?.\-_*+#*]+\.[a-zA-Z]{2,}(?:\/\S*)?"
 
-if email_match:
-    for emails in email_match:
-        print(emails)
+url_match = re.findall(url_regex, file_data)
+
+if url_match:
+    for url in url_match:
+        print(url)
 else:
-    print("No email founds")
+    print("No Urls found")

5ccab5572452c1b6445a72ca8898ad62ef955fdf
BodeMurairi2 <b.murairi@alustudent.com>
2025-05-18 12:46:24 +0200

    find phone number and credit card

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ This module uses regular expression to extract data
 import re
 
 # open form.txt
-with open("form.txt", "r") as file:
-    file_data = file.read()
+def opened_file():
+    with open("form.txt", "r") as file:
+        data = file.read()
+    return data
 
-# email regex form to extract all emails
-email_regex = r"\b[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z0-9._]{2,}\b"
+file_data = opened_file()
 
-def email_find(email_regex, file_data):
+#............................
+# Finding emails
+def email_find(file_data):
+    # email regex form to extract all emails
+    email_regex = r"\b[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z0-9._]{2,}\b"
+
     """This function display all the emails found"""
     email_match = re.findall(email_regex, file_data)
     if email_match:
@@ -22,12 +28,41 @@ def email_find(email_regex, file_data):
 
 #.........................................
 #  Finding URL
-url_regex = r"https?:\/\/(?:www\.)?[a-zA-Z0-9?.\-_*+#*]+\.[a-zA-Z]{2,}(?:\/\S*)?"
+def find_url(file_data):
+    """
+    This function find all the urls inside form.txt
+    """
+    url_regex = r"https?:\/\/(?:www\.)?[a-zA-Z0-9?.\-_*+#*]+\.[a-zA-Z]{2,}(?:\/\S*)?"
+    url_match = re.findall(url_regex, file_data)
+    if url_match:
+        for url in url_match:
+            print(url)
+    else:
+        print("No Urls found")
 
-url_match = re.findall(url_regex, file_data)
+#.............................
+# Finding phone number
+def find_phone_number(file_data):
+    """This function returns all the phone numbers"""
+    phone_number_regex = r"\+?(?:\(\d{3}\)|\d{3})[\s.-]?\d{3}[\s.-]?\d{4}"
+    phone_numbers = re.findall(phone_number_regex, file_data)
+    if phone_numbers:
+        for numbers in phone_numbers:
+            print(numbers)
+    else:
+        print("No phone numbers found!")
 
-if url_match:
-    for url in url_match:
-        print(url)
-else:
-    print("No Urls found")
+#...................................
+# Finding credit card
+def find_credit_card(file_data):
+    """This function extracts all the credit card"""
+    credit_card_regex = r"^(?:\d{4})([\s-]?\d{4}){3}$"
+    credit_card = re.findall(credit_card_regex, file_data)
+    if credit_card:
+        for card_numbers in credit_card:
+            print(card_numbers)
+    else:
+        print("No credit cards found!")
+        print(credit_card)
+
+find_credit_card(file_data)

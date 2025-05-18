62ba2e1d8095e54c2a6f6a2d0c5f49d1ee7fb59e
BodeMurairi2 <b.murairi@alustudent.com>
2025-05-18 14:55:26 +0200

    Regex Complete, CLI setup

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 This module uses regular expression to extract data
 """
 import re
+import time
+
 
 # open form.txt
 def opened_file():
@@ -10,11 +12,9 @@ def opened_file():
         data = file.read()
         return data
 
-file_data = opened_file()
-
 #............................
 # Finding emails
-def email_find(file_data):
+def find_email(file_data):
     # email regex form to extract all emails
     email_regex = r"\b[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z0-9._]{2,}\b"
 
@@ -104,6 +104,64 @@ def find_hashtags(file_data):
 
 #......................................................
 # Finding html tags
-html_regex = r"<\/?[a-zA-Z][a-zA-Z0-9]*\b[^>]*\/?>"
-htmls = re.findall(html_regex, file_data)
-print(htmls)
+def find_htmls(file_data):
+    html_regex = r"<\/?[a-zA-Z][a-zA-Z0-9]*\b[^>]*\/?>"
+    htmls = re.findall(html_regex, file_data)
+    if htmls:
+        for html in htmls:
+            print(html)
+    else:
+        print("No htmls tags found")
+
+if __name__ == "__main__":
+    """Running the application"""
+    while True:
+        # open the file
+        file_data = opened_file()
+        print("Welcome to our Regex pattern matching plateform\n")
+        print("Here you can extract contents in your file\n")
+        print("________________________________________")
+        print("1.Find Emails inside your file")
+        print("2.Find urls inside your file")
+        print("3.Find phone numbers inside your file")
+        print("4.Find credit cards inside your file")
+        print("5.Find a/ll the times")
+        print("6.Find amount/currency inside your file")
+        print("7.Find hashtags inside your file")
+        print("8.Find htmls tags inside your file")
+        print("9.Exit!")
+        print("___________________________________________")
+        
+        choice = int(input("Enter your choice between 1-9:\n"))
+        
+        if choice == 1:
+            print("All Emails inside your file:\n")
+            time.sleep(1)
+            find_email(file_data)
+        elif choice == 2:
+            print("All urls inside your file")
+            time.sleep(1)
+            find_url(file_data)
+        elif choice == 3:
+            print("All phone numbers inside your file")
+            time.sleep(1)
+            find_phone_number(file_data)
+        elif choice == 4:
+
+        elif choice == 5:
+
+        elif choice == 6:
+
+        elif choice == 7:
+
+        elif choice == 8:
+
+        elif choice > 9:
+            print("Exiting the application...\n")
+            time.sleep(1)
+            break
+
+        else:
+            print("Error! Please enter a valid number between 1-9 not decimal nor letters")
+            continue
+        
